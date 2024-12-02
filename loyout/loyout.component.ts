55346023253d7ae840c3a7b7bc6bde2ad05fb64d
Janushan Anantharasa <88024116+A-Janushan@users.noreply.github.com>
2024-12-02 20:18:48 +0530

    Add files via upload

---
+++ loyout/loyout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loyout',
+  imports: [],
+  templateUrl: './loyout.component.html',
+  styleUrl: './loyout.component.css'
+})
+export class LoyoutComponent {
+
+}
